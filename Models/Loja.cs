264cdd082279663cae7256f9e81fdd46a06bd7c2
ol1ve1ravj <joaogggg765@gmail.com>
2024-03-31 20:31:27 -0300

    joao

---
+++ Models/Loja.cs
@@ -0,0 +1,12 @@
+class Loja{
+    public string Products { get; set; }
+
+    public string Location { get; set; }
+
+    public int Id { get; set; }
+
+    public string Category { get; set; }
+
+    public int ClientsNumbers { get; set; }
+
+}
